05398653d21dc3046827c018b01a791c19d9c7f1
Stevens Brito <31490578+stevensdotb@users.noreply.github.com>
2025-06-22 03:43:45 +0200

    Change reload value

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from contextlib import asynccontextmanager
+from doctest import debug
 
 from fastapi import FastAPI
 
@@ -32,4 +33,4 @@ app.include_router(router, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=settings.DEBUG)
